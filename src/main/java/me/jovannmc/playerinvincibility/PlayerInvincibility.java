23c14d5386cfa871de55a96dc28a54c49d663ee1
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-12-22 16:34:46 +0300

    1.1.0
    I forgot to commit 1.0.0 so that's why the time between is so short lol

--- src/main/java/me/jovannmc/playerinvincibility/PlayerInvincibility.java
+++ src/main/java/me/jovannmc/playerinvincibility/PlayerInvincibility.java
@@ -1,5 +1,6 @@
 package me.jovannmc.playerinvincibility;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +12,11 @@ public final class PlayerInvincibility extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        if (getConfig().getInt("configVersion") != 1) {
+            getLogger().warning("Outdated config version! Please delete your config.yml and restart the server!");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("PlayerInvincibility has been enabled!");
         getLogger().info("Invincible players: " + getConfig().getStringList("invincible").toString());
@@ -26,8 +32,18 @@ public final class PlayerInvincibility extends JavaPlugin implements Listener {
         // Check config if player is in "invincible", if on the list and player was damaged by another player, cancel the event
         if (event.getEntity() instanceof Player && getConfig().getStringList("invincible").contains(event.getEntity().getName()) && event.getCause() == EntityDamageByEntityEvent.DamageCause.ENTITY_ATTACK && event.getDamager() instanceof Player) {
             event.setCancelled(true);
-            getLogger().info("Player " + event.getEntity().getName() + " was attempted to be damaged by " + event.getDamager().getName() + " but was invincible!");
-            event.getEntity().sendMessage("You were attempted to be damaged by " + event.getDamager().getName() + " but you are invincible!");
+            if (getConfig().getBoolean("logInvincibleMessage")) {
+                getLogger().info("Player " + event.getEntity().getName() + " was attempted to be damaged by " + event.getDamager().getName() + " but was invincible!");
+            }
+
+            if (getConfig().getBoolean("sendInvincibleMessage")) {
+                event.getEntity().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("invincibleMessage")).replace("%player%", event.getDamager().getName()));
+                event.getDamager().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("invincibleMessageDamager")).replace("%player%", event.getEntity().getName()));
+            }
+
+            if (getConfig().getBoolean("broadcastInvincibleMessage")) {
+                getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("invincibleMessageBroadcast")).replace("%player%", event.getEntity().getName()).replace("%damager%", event.getDamager().getName()));
+            }
         }
     }
 }
